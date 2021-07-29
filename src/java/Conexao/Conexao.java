82ee384478c0dfad7c66fe4d9bcdd8ec95b8ec1d
IzabelaSoares <soares.izabelamaria@gmail.com>
2021-07-29 15:29:52 -0300

    Emissão Inicial

---
+++ src/java/Conexao/Conexao.java
@@ -0,0 +1,20 @@
+package Conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static Connection conectar(){
+        Connection con = null;
+        String url = "jdbc:postgresql://localhost:5432/breakfest";
+        String user = "postgres";
+        String password = "baunilha";
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Erro ao conectar com o banco de dados");
+        }
+        return con;                
+    }    
+}
