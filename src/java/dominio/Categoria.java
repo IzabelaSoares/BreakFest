31c6e9bffb9af9858f4aa53d110db05fe2160d5c
mariareboco <mariaclaraperon@gmail.com>
2021-08-02 17:07:21 -0300

    Maria: alteração da classe Categoria (adicionando métodos principais)

--- src/java/dominio/Categoria.java
+++ src/java/dominio/Categoria.java
@@ -2,10 +2,38 @@
 
 package dominio;
 
+import bancodedados.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
     //criação de variáveis
     private Integer idCategoria;
     private String tituloCategoria;
+    
+    public List<Categoria> consultaGeral(){
+        List<Categoria> lista = new ArrayList<>();
+        String sql = "select * from categoria";
+        Connection con = Conexao.conectar();
+        try{
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                Categoria cat = new Categoria();
+                cat.setTituloCategoria(rs.getString("titulocategoria"));
+                lista.add(cat);
+            }
+        } catch (SQLException ex) {
+          System.out.println("Erro:" + ex.getMessage());
+        }
+        
+        return lista;
+        
+    }
 
     //getters e setters
     public int getIdCategoria() {
