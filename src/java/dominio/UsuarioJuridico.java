059b56057624eb1ad0eadc6e7fc00bd0dc6aed48
IzabelaSoares <soares.izabelamaria@gmail.com>
2021-08-05 20:43:36 -0300

    Izabela: Testado e aprovado login pessoa fisica (front, back e database)

---
+++ src/java/dominio/UsuarioJuridico.java
@@ -0,0 +1,166 @@
+//@author Maria
+package dominio;
+
+import bancodedados.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioJuridico {
+    //declaração de variáveis
+    private int    idUsuario;
+    private String nickUsuario;
+    private String email;
+    private String senha;
+    
+    //métodos
+    public static boolean podeLogar(String email, String senha){
+        Connection con = Conexao.conectar();
+        String sql = "select * from usuario where fkemail = ? and senha = ?";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, email);
+            stm.setString(2, senha);
+            ResultSet rs = stm.executeQuery();
+            return rs.next();            
+        } 
+            catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            }
+        return true;
+    }  
+    
+    /* Verifica se login do usuário existe para validação do usuário */
+
+    public boolean verificaExistencia(String email){
+        Connection con = Conexao.conectar();
+        String sql = "select * from usuario where fkemail = ?";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, email);
+            ResultSet rs = stm.executeQuery();
+            return rs.next();            
+        } 
+            catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            }
+        return true;
+    }  
+
+    public boolean cadastrarUsuario(){
+        String sql = "insert into usuariofisico(fkemail,senha) values(?,?)";
+        Connection con = Conexao.conectar();
+       
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, this.email);
+            stm.setString(2, this.senha);
+            stm.execute();           
+        }    catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return false;
+            }        
+        return true;
+   }
+
+    public boolean alterarUsuario(){
+        Connection con = Conexao.conectar();
+        String   sql = "update usuario set fkemail = ?, senha = ? where idusuario = ?";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, this.email);
+            stm.setString(2, this.senha);    
+            stm.setInt(3, this.idUsuario);
+            stm.execute();           
+        } 
+            catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return false;
+            }        
+       return true;
+    }
+   
+    public boolean excluirUsuario(){
+        Connection con = Conexao.conectar();
+        String sql = "delete from usuario where idusuario = ?";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, this.idUsuario);
+            stm.execute();           
+       } 
+            catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return false;
+            }        
+       return true;
+    }
+   
+    public UsuarioJuridico consultarUsuario(String email){
+        Connection con = Conexao.conectar();
+        String sql = "select fkemail, senha from usuario where fkemail = ?";
+        UsuarioJuridico login = null;
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, email);
+            ResultSet rs = stm.executeQuery();
+            if(rs.next()){
+                login = new UsuarioJuridico();
+                login.setEmail(rs.getString("fkemail"));
+                login.setSenha(rs.getString("senha"));
+            }
+        } 
+            catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }      
+        return login;  
+    }
+   
+    public List<UsuarioJuridico> consultar(){
+        List<UsuarioJuridico> lista = new ArrayList<>();
+        Connection con = Conexao.conectar();
+        String sql  = "select idusuario, fkemail from usuario order by idusuario";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                UsuarioJuridico login = new UsuarioJuridico();
+                login.setIdUsuario(rs.getInt("idusuario"));
+                login.setEmail(rs.getString("fkemail"));
+                lista.add(login);
+            }
+        } 
+            catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }      
+        return lista;
+    }
+    
+    //getters e setters  
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String Email) {
+        this.email = Email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.senha = Senha;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+}
--- web/recebeclientefisico.jsp
+++ web/recebeclientefisico.jsp
@@ -1,47 +1,41 @@
 <%-- 
-    Document   : recebeclientefisico
-    Created on : 03/08/2021, 08:25:16
-    Author     : Ricardo
+    Author     : Ricardo e Izabela
 --%>
+<%@page import="dominio.UsuarioFisico"%>
 <%@page import="java.text.SimpleDateFormat"%>
-<%@page import="dominio.Login"%>
 <%@page import="java.sql.Date"%>
 <%@page import="dominio.PessoaFisica"%>
-        <%  //instancia a pessoa física = PF
-            PessoaFisica pf = new PessoaFisica();
-            
-            //transformar a data para entrar no banco de dados
-            SimpleDateFormat formatarDate = new SimpleDateFormat("yyyy-MM-dd");
-            
-            //recebe os valores da tela HTML
-            pf.setNome(request.getParameter("nome"));
-            pf.setSobrenome(request.getParameter("sobrenome"));
-            pf.setCpf(request.getParameter("cpf"));
-            pf.setDataNascimento(Date.valueOf(request.getParameter("nascimento")));
-            pf.setEmail(request.getParameter("email"));
-            pf.setCelular(request.getParameter("telefone"));
-            pf.setCep(request.getParameter("cep"));
-            pf.setEstado(request.getParameter("UF"));
-            pf.setCidade(request.getParameter("cidade"));
-            pf.setBairro(request.getParameter("bairro"));
-            pf.setRua(request.getParameter("rua"));
-            pf.setNumero(Integer.parseInt(request.getParameter("numero")));
-            pf.setComplemento(request.getParameter("complemento"));            
-            
-            //se cadastrar a pessoa fisica
-            if(pf.cadastrarConta()){
-                //instanciar o login da = PF
-                Login login = new Login();
-                
-                //Passar valores da tela e cadastrar o usuário
-                login.setEmail(request.getParameter("email"));
-                login.setSenha(request.getParameter("senha"));
-                login.cadastarUsuario();
-                
-                response.sendRedirect("cadastrofisico.jsp?pmensagem=Cliente-salvo-com-sucesso");
-            } else {
-                response.sendRedirect("cadastrofisico.jsp?pmensagem=Problemas-ao-editar-cliente");
-            }                  
-        %>        
+<%  //instancia a pessoa física = PF
+    PessoaFisica pf = new PessoaFisica();
+
+    //recebe os valores da tela HTML
+    pf.setNome(request.getParameter("nome"));
+    pf.setSobrenome(request.getParameter("sobrenome"));
+    pf.setCpf(request.getParameter("cpf"));
+    pf.setDataNascimento(Date.valueOf(request.getParameter("nascimento")));
+    pf.setEmail(request.getParameter("email"));
+    pf.setCelular(request.getParameter("telefone"));
+    pf.setCep(request.getParameter("cep"));
+    pf.setEstado(request.getParameter("UF"));
+    pf.setCidade(request.getParameter("cidade"));
+    pf.setBairro(request.getParameter("bairro"));
+    pf.setRua(request.getParameter("rua"));
+    pf.setNumero(Integer.parseInt(request.getParameter("numero")));
+    pf.setComplemento(request.getParameter("complemento"));
+
+    //instanciar o login da = PF
+    UsuarioFisico login = new UsuarioFisico();
+
+    //Passar valores da tela e cadastrar o usuário
+    login.setEmail(request.getParameter("email"));
+    login.setSenha(request.getParameter("senha"));
+
+    //se cadastrar pessoa e o login dela
+    if (pf.cadastrarConta() && login.cadastrarUsuario()) {
+        response.sendRedirect("cadastrofisico.jsp?pmensagem=Cliente-salvo-com-sucesso");
+    } else {
+        response.sendRedirect("cadastrofisico.jsp?pmensagem=Problemas-ao-editar-cliente");
+    }
+%>        
 
 
