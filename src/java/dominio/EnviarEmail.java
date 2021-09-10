a0c1f7b96026c2dad905e092ab79f9d55a5d3f8f
mariareboco <mariaclaraperon@gmail.com>
2021-09-10 16:51:37 -0300

    Maria: trocando gif do email

--- src/java/dominio/EnviarEmail.java
+++ src/java/dominio/EnviarEmail.java
@@ -105,7 +105,7 @@ public class EnviarEmail {
           message.setRecipients(Message.RecipientType.TO, toUser);
           message.setSubject("Recupere sua senha!");//Assunto
           message.setContent(
-              "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\" style=\"width:100%;font-family:helvetica, 'helvetica neue', arial, verdana, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0\">\n" +
                 "<head>\n" +
                 "<meta charset=\"UTF-8\">\n" +
@@ -185,14 +185,6 @@ public class EnviarEmail {
                 "background:transparent!important;\n" +
                 "border-style:dashed dashed dashed dashed!important;\n" +
                 "}\n" +
-                "@media only screen and (max-device-width:600px) {\n" +
-                ".es-content table,\n" +
-                ".es-header table,\n" +
-                ".es-footer table {\n" +
-                "width:100%!important;\n" +
-                "max-width:600px!important;\n" +
-                "}\n" +
-                "}\n" +
                 "@media only screen and (max-width:600px) {p, ul li, ol li, a { line-height:150%!important } h1, h2, h3, h1 a, h2 a, h3 a { line-height:120%!important } h1 { font-size:20px!important; text-align:center } h2 { font-size:16px!important; text-align:left } h3 { font-size:20px!important; text-align:center } .es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a { font-size:20px!important } h2 a { text-align:left } .es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a { font-size:16px!important } .es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a { font-size:20px!important } .es-menu td a { font-size:14px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:10px!important } .es-content-body p, .es-content-body ul li, .es-content-body ol li, .es-content-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:12px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class=\"gmail-fix\"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button, button.es-button { font-size:14px!important; display:block!important; border-left-width:0px!important; border-right-width:0px!important } .es-btn-fw { border-width:10px 0px!important; text-align:center!important } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }\n" +
                 "</style>\n" +
                 "</head>\n" +
@@ -218,7 +210,7 @@ public class EnviarEmail {
                 "<td class=\"es-m-p0r es-m-p20b\" align=\"center\" style=\"padding:0;Margin:0;width:83px\">\n" +
                 "<table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" role=\"presentation\" style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px\">\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
-                "<td align=\"center\" style=\"padding:0;Margin:0;font-size:0px\"><a target=\"_blank\" href=\"http://localhost:8080/BreakFest/\" style=\"-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:none;color:#0B5394;font-size:16px\"><img src=\"https://lsegqi.stripocdn.email/content/guids/e68d3e5d-9712-4ac5-afe4-d5dc612770d9/images/61721631211274906.gif\" alt=\"BREAKFEST\" style=\"display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic\" width=\"83\" title=\"BREAKFEST\"></a></td>\n" +
+                "<td align=\"center\" style=\"padding:0;Margin:0;font-size:0px\"><a target=\"_blank\" href=\"http://localhost:8080/BreakFest/\" style=\"-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:none;color:#0B5394;font-size:16px\"><img src=\"https://lsegqi.stripocdn.email/content/guids/CABINET_0c817cf96fa6e63bbef1b82c35b627b6/images/55911631293045501.gif\" alt=\"BREAKFEST\" style=\"display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic\" width=\"83\" title=\"BREAKFEST\"></a></td>\n" +
                 "</tr>\n" +
                 "</table></td>\n" +
                 "</tr>\n" +
@@ -250,13 +242,13 @@ public class EnviarEmail {
                 "<td valign=\"top\" align=\"center\" style=\"padding:0;Margin:0;width:600px\">\n" +
                 "<table style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-position:left top\" width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" role=\"presentation\">\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
-                "<td align=\"center\" style=\"padding:0;Margin:0;padding-top:5px;padding-bottom:5px;font-size:0px\"><img src=\"https://lsegqi.stripocdn.email/content/guids/e68d3e5d-9712-4ac5-afe4-d5dc612770d9/images/18281631212050650.gif\" alt style=\"display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic\" width=\"190\"></td>\n" +
+                "<td align=\"center\" style=\"padding:0;Margin:0;padding-top:5px;padding-bottom:5px;font-size:0px\"><img src=\"https://lsegqi.stripocdn.email/content/guids/CABINET_0c817cf96fa6e63bbef1b82c35b627b6/images/65711631302463083.gif\" alt style=\"display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic\" width=\"190\"></td>\n" +
                 "</tr>\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
                 "<td align=\"center\" style=\"padding:0;Margin:0;padding-top:15px;padding-bottom:15px\"><h1 style=\"Margin:0;line-height:24px;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;font-size:20px;font-style:normal;font-weight:normal;color:#16120d\"><b>ESQUECEU SUA<br>SENHA?</b></h1></td>\n" +
                 "</tr>\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
-                "<td align=\"center\" style=\"padding:0;Margin:0;padding-left:40px;padding-right:40px\"><p style=\"Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:helvetica, 'helvetica neue', arial, verdana, sans-serif;line-height:24px;color:#666666;font-size:16px\">OLÁ,&nbsp;,"+ EnviarEmail.procuraNome(pEmail) +" </p></td>\n" +
+                "<td align=\"center\" style=\"padding:0;Margin:0;padding-left:40px;padding-right:40px\"><p style=\"Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:helvetica, 'helvetica neue', arial, verdana, sans-serif;line-height:24px;color:#666666;font-size:16px\">OLÁ,&nbsp;"+ EnviarEmail.procuraNome(pEmail) +",</p></td>\n" +
                 "</tr>\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
                 "<td align=\"center\" style=\"padding:0;Margin:0;padding-right:35px;padding-left:40px\"><p style=\"Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:helvetica, 'helvetica neue', arial, verdana, sans-serif;line-height:24px;color:#666666;font-size:16px\">Recebemos um pedido para alteração da senha se sua conta!</p></td>\n" +
@@ -265,14 +257,14 @@ public class EnviarEmail {
                 "<td align=\"center\" style=\"padding:0;Margin:0;padding-top:25px;padding-left:40px;padding-right:40px\"><p style=\"Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:helvetica, 'helvetica neue', arial, verdana, sans-serif;line-height:24px;color:#666666;font-size:16px\">Se não foi você, apenas ignore este e-mail. Se não, clique no botão abaixo para recuperar sua senha:</p></td>\n" +
                 "</tr>\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
-                "<td align=\"center\" style=\"Margin:0;padding-left:10px;padding-right:10px;padding-top:40px;padding-bottom:40px\"><span class=\"es-button-border es-button-border-2\" style=\"border-style:solid;border-color:#88500a;background:#ffffff;border-width:2px;display:inline-block;border-radius:10px;width:auto\"><a href=\"http://localhost:8080/BreakFest/recuperarsenha.jsp\" class=\"es-button es-button-1\" target=\"_blank\" style=\"mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#88500a;font-size:14px;border-style:solid;border-color:#FFFFFF;border-width:15px 20px 15px 20px;display:inline-block;background:#FFFFFF;border-radius:10px;font-family:arial, 'helvetica neue', helvetica, sans-serif;font-weight:bold;font-style:normal;line-height:17px;width:auto;text-align:center\">REDEFINA SUA SENHA</a></span></td>\n" +
+                "<td align=\"center\" style=\"Margin:0;padding-left:10px;padding-right:10px;padding-top:40px;padding-bottom:40px\"><span class=\"es-button-border-2 es-button-border\" style=\"border-style:solid;border-color:#88500a;background:#ffffff;border-width:2px;display:inline-block;border-radius:10px;width:auto\"><a href=\"http://localhost:8080/BreakFest/recuperarsenha.jsp\" class=\"es-button es-button-1\" target=\"_blank\" style=\"mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#88500a;font-size:14px;border-style:solid;border-color:#FFFFFF;border-width:15px 20px 15px 20px;display:inline-block;background:#FFFFFF;border-radius:10px;font-family:arial, 'helvetica neue', helvetica, sans-serif;font-weight:bold;font-style:normal;line-height:17px;width:auto;text-align:center\">REDEFINA SUA SENHA</a></span></td>\n" +
                 "</tr>\n" +
                 "</table></td>\n" +
                 "</tr>\n" +
                 "</table></td>\n" +
                 "</tr>\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
-                "<td style=\"Margin:0;padding-top:5px;padding-bottom:20px;padding-left:20px;padding-right:20px;background-position:left top\" align=\"left\">\n" +
+                "<td align=\"left\" style=\"Margin:0;padding-top:5px;padding-bottom:20px;padding-left:20px;padding-right:20px\">\n" +
                 "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" style=\"mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px\">\n" +
                 "<tr style=\"border-collapse:collapse\">\n" +
                 "<td valign=\"top\" align=\"center\" style=\"padding:0;Margin:0;width:560px\">\n" +
